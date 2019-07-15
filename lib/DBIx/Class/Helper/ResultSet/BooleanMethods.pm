9b28d2afa7c1b17bc667645d9e56ecc153ca134c
Mathieu Arnold <mat@mat.cc>
2019-07-15 15:56:53 +0200

    Bump to 0.05.

--- lib/DBIx/Class/Helper/ResultSet/BooleanMethods.pm
+++ lib/DBIx/Class/Helper/ResultSet/BooleanMethods.pm
@@ -4,7 +4,7 @@ use 5.008;
 use utf8;
 use strictures 2;
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 =head1 NAME
 
@@ -12,7 +12,7 @@ DBIx::Class::Helper::ResultSet::BooleanMethods - Automatically create search met
 
 =head1 VERSION
 
-Version 0.04
+Version 0.05
 
 =cut
 
